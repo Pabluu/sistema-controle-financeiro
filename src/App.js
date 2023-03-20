23b25bbea6747c442324652d88790ee4ee43c00a
Pablo Henrique SC <pabloph0077@gmail.com>
2023-03-20 11:20:17 -0300

    add: Finalizando Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import GlobalStyle from "./styles/global";
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <>
-      <div>App</div>
+      <Header />
       <GlobalStyle />
     </>
   );
