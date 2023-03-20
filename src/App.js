04f728f7480db5ebed209af67c856614f95a31ba
Pablo Henrique SC <pabloph0077@gmail.com>
2023-03-20 14:11:44 -0300

    add: trabalhando com o formulario

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import GlobalStyle from "./styles/global";
 import Header from "./components/Header";
 import Resume from "./components/Resume";
+import Form from "./components/Form";
 
 export default function App() {
   return (
