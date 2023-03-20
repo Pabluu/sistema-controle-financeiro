b3841690609d928891bd0acd64d9d60db332ddbc
Pablo Henrique SC <pabloph0077@gmail.com>
2023-03-20 12:48:31 -0300

    add: estrutura da pagina

--- src/components/ResumeItem/index.js
+++ src/components/ResumeItem/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import * as C from "./styles";
 
-const ResumeItem = () => {
+const ResumeItem = ({ title, Icon, value }) => {
   return (
     <C.Container>
       <C.Header>
-        <C.HeaderTitle>teste</C.HeaderTitle>
+        <C.HeaderTitle>{title}</C.HeaderTitle>
+        <Icon />
       </C.Header>
       <C.Total>1000</C.Total>
     </C.Container>
