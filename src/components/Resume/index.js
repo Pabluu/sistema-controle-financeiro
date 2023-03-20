3ff4f7f14e3279c1c949c990a1c1602fd691afa7
Pablo Henrique SC <pabloph0077@gmail.com>
2023-03-20 12:37:32 -0300

    add: estrutura da pagina

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,8 +1,15 @@
 import React from "react";
+import ResumeItem from "../ResumeItem";
 import * as C from "./styles";
 
 const Resume = () => {
-  return <C.Container>Teste</C.Container>;
+  return (
+    <C.Container>
+      <ResumeItem>Resume</ResumeItem>
+      <ResumeItem>Resume</ResumeItem>
+      <ResumeItem>Resume</ResumeItem>
+    </C.Container>
+  );
 };
 
 export default Resume;
